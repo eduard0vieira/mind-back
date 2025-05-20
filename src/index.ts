2fb0cca30f50b68bcaf8c5029459523236ee4d14
eduard0vieira <eduardohvieira05@gmail.com>
2025-05-20 13:58:10 -0300

    feat: add prisma and connect to mysql; create register route

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
-import path from 'path';
-import { log } from 'console';
+import authRoutes from './routes/auth.routes';
 
 dotenv.config()
 
@@ -13,12 +12,13 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.get('/', (req, res) => {
   res.send('API rodando 🚀')
 });
 
+app.use('/api/auth', authRoutes);
+
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT} ✅`);
 })
---
+++ src/services/auth.service.ts
@@ -0,0 +1,34 @@
+import bcrypt from 'bcrypt';
+import prisma from '../config/prisma';
+
+interface RegisterDTO {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export const registerUser = async ({ name, email, password }: RegisterDTO) => {
+  const existingUser = await prisma.user.findUnique({ where: { email } });
+
+  if (existingUser) {
+    throw new Error('Email já cadastrado.');
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      createdAt: true,
+    },
+  });
+
+  return user;
+};
