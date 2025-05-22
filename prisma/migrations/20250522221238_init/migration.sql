e04c78a67fbcdee08a869d1495c0181aa7892b58
eduard0vieira <eduardohvieira05@gmail.com>
2025-05-22 19:32:28 -0300

    fix: auth problems with user trying to post article

--- prisma/migrations/20250522221238_init/migration.sql
+++ prisma/migrations/20250522221238_init/migration.sql
@@ -16,7 +16,7 @@ CREATE TABLE `Article` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(191) NOT NULL,
     `content` VARCHAR(191) NOT NULL,
-    `image` VARCHAR(191) NULL,
+    `image` TEXT NULL,
     `authorId` INTEGER NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
--- src/services/articles/article.service.ts
+++ src/services/articles/article.service.ts
@@ -4,16 +4,38 @@ interface ArticleInput {
   title: string;
   content: string;
   authorId: number;
-  image?: string; 
+  image?: string;
 }
 
 export const createArticleService = async (data: ArticleInput) => {
-  const articleData = {
-    ...data
-  };
+  const { title, content, image, authorId } = data;
 
-  return await prisma.article.create({ data: articleData });
+  const authorIdNumber = Number(authorId);
+  if (isNaN(authorIdNumber)) {
+    throw new Error("authorId inválido");
+  }
+
+  const userExists = await prisma.user.findUnique({
+    where: { id: authorIdNumber },
+  });
+
+  if (!userExists) {
+    throw new Error("authorId inválido: usuário não encontrado");
+  }
+
+  return await prisma.article.create({
+    data: {
+      title,
+      content,
+      image,
+      authorId: authorIdNumber,
+    },
+    include: {
+      author: true,
+    },
+  });
 };
+
 
 export const getAllArticlesService = async () => {
   return await prisma.article.findMany({ include: { author: true } });
@@ -33,6 +55,7 @@ export const updateArticleService = async (
   return await prisma.article.update({
     where: { id },
     data,
+    include: { author: true },
   });
 };
 
